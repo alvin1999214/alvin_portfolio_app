f19f6667f6c5b137ff18e434d77b2b0fc0293bdb
Alvin Lau <alvinlau@esdlife.com>
2022-11-19 00:23:56 +0800

    Personal info page code formating

--- lib/screens/personal_info.dart
+++ lib/screens/personal_info.dart
@@ -8,10 +8,9 @@ class PersonalPage extends StatefulWidget {
 
   @override
   State<PersonalPage> createState() => _PersonalPageState();
-
 }
 
-class _PersonalPageState extends State<PersonalPage>{
+class _PersonalPageState extends State<PersonalPage> {
   PersonalInfoModel? mInfoObj;
   bool? loading;
 
@@ -40,72 +39,32 @@ class _PersonalPageState extends State<PersonalPage>{
                   collapsedHeight: 40,
                   expandedHeight: 300,
                   paddingTop: MediaQuery.of(context).padding.top,
-                  coverImgUrl: 'https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg'
-              ),
+                  coverImgUrl:
+                      'https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg'),
             ),
             SliverFillRemaining(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 10),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    SizedBox(height: 10),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(6),
-                          child: Image.network(
-                            'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png',
-                            width: 130,
-                            height: 180,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Padding(padding: EdgeInsets.only(left: 16)),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              mInfoObj!.info.name,
-                              style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFF333333),
-                              ),
-                            ),
-                            Padding(padding: EdgeInsets.only(top: 10)),
-                            Text(
-                              "Phone: ${mInfoObj!.info.phone}",
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Color(0xFF999999),
-                              ),
-                            ),
-                            Padding(padding: EdgeInsets.only(top: 2)),
-                            Text(
-                              "Address: ${mInfoObj!.info.address}",
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Color(0xFF999999),
-                              ),
-                            ),
-                            Padding(padding: EdgeInsets.only(top: 2)),
-                            Text(
-                              "Github: ${mInfoObj!.info.github}",
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Color(0xFF999999),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(6),
+                      child: Image.network(
+                        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png',
+                        width: 130,
+                        height: 180,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                    Divider(height: 32),
+                    Padding(padding: EdgeInsets.only(left: 16)),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Text(
-                          'Career Objective',
+                          mInfoObj!.info.name,
                           style: TextStyle(
                             fontSize: 25,
                             fontWeight: FontWeight.bold,
@@ -114,8 +73,23 @@ class _PersonalPageState extends State<PersonalPage>{
                         ),
                         Padding(padding: EdgeInsets.only(top: 10)),
                         Text(
-                          "This is my objective",
-                          textAlign: TextAlign.justify,
+                          "Phone: ${mInfoObj!.info.phone}",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Color(0xFF999999),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 2)),
+                        Text(
+                          "Address: ${mInfoObj!.info.address}",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Color(0xFF999999),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 2)),
+                        Text(
+                          "Github: ${mInfoObj!.info.github}",
                           style: TextStyle(
                             fontSize: 15,
                             color: Color(0xFF999999),
@@ -123,26 +97,49 @@ class _PersonalPageState extends State<PersonalPage>{
                         ),
                       ],
                     ),
-                    Divider(height: 32),
                   ],
-                )
-            )
+                ),
+                Divider(height: 32),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Career Objective',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF333333),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    Text(
+                      mInfoObj!.careerObjective,
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color(0xFF999999),
+                      ),
+                    ),
+                  ],
+                ),
+                Divider(height: 32),
+              ],
+            ))
           ],
         ),
       );
-  }else{
+    } else {
       return Scaffold(
         body: const Center(
           child: Text('Loading...',
               style: TextStyle(
                 color: Color(0xff172633),
                 fontSize: 20,
-              )
-          ),
+              )),
         ),
       );
     }
-    }
+  }
 }
 
 class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
@@ -173,13 +170,13 @@ class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
   }
 
   void updateStatusBarBrightness(shrinkOffset) {
-    if(shrinkOffset > 50 && this.statusBarMode == 'dark') {
+    if (shrinkOffset > 50 && this.statusBarMode == 'dark') {
       this.statusBarMode = 'light';
       SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
         statusBarBrightness: Brightness.light,
         statusBarIconBrightness: Brightness.light,
       ));
-    } else if(shrinkOffset <= 50 && this.statusBarMode == 'light') {
+    } else if (shrinkOffset <= 50 && this.statusBarMode == 'light') {
       this.statusBarMode = 'dark';
       SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
         statusBarBrightness: Brightness.dark,
@@ -189,21 +186,26 @@ class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
   }
 
   Color makeStickyHeaderBgColor(shrinkOffset) {
-    final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
+    final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255)
+        .clamp(0, 255)
+        .toInt();
     return Color.fromARGB(alpha, 255, 255, 255);
   }
 
   Color makeStickyHeaderTextColor(shrinkOffset, isIcon) {
-    if(shrinkOffset <= 50) {
+    if (shrinkOffset <= 50) {
       return isIcon ? Colors.white : Colors.transparent;
     } else {
-      final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
+      final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255)
+          .clamp(0, 255)
+          .toInt();
       return Color.fromARGB(alpha, 0, 0, 0);
     }
   }
 
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
     this.updateStatusBarBrightness(shrinkOffset);
     return Container(
       height: this.maxExtent,
@@ -246,7 +248,8 @@ class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
                       IconButton(
                         icon: Icon(
                           Icons.arrow_back_ios,
-                          color: this.makeStickyHeaderTextColor(shrinkOffset, true),
+                          color: this
+                              .makeStickyHeaderTextColor(shrinkOffset, true),
                         ),
                         onPressed: () => Navigator.pop(context),
                       ),
@@ -255,13 +258,15 @@ class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
                         style: TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.w500,
-                          color: this.makeStickyHeaderTextColor(shrinkOffset, false),
+                          color: this
+                              .makeStickyHeaderTextColor(shrinkOffset, false),
                         ),
                       ),
                       IconButton(
                         icon: Icon(
                           Icons.whatsapp,
-                          color: this.makeStickyHeaderTextColor(shrinkOffset, false),
+                          color: this
+                              .makeStickyHeaderTextColor(shrinkOffset, false),
                         ),
                         onPressed: () {},
                       ),
