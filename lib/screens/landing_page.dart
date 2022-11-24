6470dc25c703fc6ce190fdd93f0cbcd97d985312
Alvin Lau <>
2022-11-24 20:58:30 +0800

    close keyboard by tap out site text field

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -31,101 +31,104 @@ class _LandingPageState extends State<LandingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("Login Page"),
-        backgroundColor: Color(0xff172633),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: Center(
-                child: Container(
-                    width: 200,
-                    height: 150,
-                    /*decoration: BoxDecoration(
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text("Login Page"),
+          backgroundColor: Color(0xff172633),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0),
+                child: Center(
+                  child: Container(
+                      width: 200,
+                      height: 150,
+                      /*decoration: BoxDecoration(
                         color: Colors.red,
                         borderRadius: BorderRadius.circular(50.0)),*/
-                    child: Icon(
-                      Icons.flutter_dash,
-                      size: 100,
-                    )),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Alvin's Portfolio app",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                controller: accessKeyController,
-                decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0xff172633)
-                      )
-                    ),
-                    labelText: 'Access Key',
-                    labelStyle: TextStyle(
-                      color: Color(0xff172633)
-                    ),
-                    hintText: ''),
-                cursorColor: Color(0xff172633),
-              ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 55.0),
-              decoration: BoxDecoration(
-                  color: Color(0xff172633), borderRadius: BorderRadius.circular(20)),
-              child: TextButton(
-                onPressed: () {
-                  // Navigator.push(
-                  //     context, MaterialPageRoute(builder: (_) => HomePage()));
-                  if (accessKeyController.text.isNotEmpty) {
-                    fetchMobileConfig(accessKeyController.text).then((value) => setState(() {
-                      if (value){
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),));
-                      }else{
-                        showAccessFailDialog(context);
-                      }
-                    }));
-                  } else {
-                    showEmptyAccessKeyDialog(context);
-                  }
-                },
-                child: Text(
-                  'Get Access',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
+                      child: Icon(
+                        Icons.flutter_dash,
+                        size: 100,
+                      )),
                 ),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                //TODO Scan QR code
-              },
-              child: Text(
-                'Access by QR code',
-                style: TextStyle(color: Color(0xff172633), fontSize: 15),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Text(
+                  "Alvin's Portfolio app",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 130,
-            ),
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  controller: accessKeyController,
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color(0xff172633)
+                          )
+                      ),
+                      labelText: 'Access Key',
+                      labelStyle: TextStyle(
+                          color: Color(0xff172633)
+                      ),
+                      hintText: ''),
+                  cursorColor: Color(0xff172633),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 55.0),
+                decoration: BoxDecoration(
+                    color: Color(0xff172633), borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: () {
+                    // Navigator.push(
+                    //     context, MaterialPageRoute(builder: (_) => HomePage()));
+                    if (accessKeyController.text.isNotEmpty) {
+                      fetchMobileConfig(accessKeyController.text).then((value) => setState(() {
+                        if (value){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),));
+                        }else{
+                          showAccessFailDialog(context);
+                        }
+                      }));
+                    } else {
+                      showEmptyAccessKeyDialog(context);
+                    }
+                  },
+                  child: Text(
+                    'Get Access',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  //TODO Scan QR code
+                },
+                child: Text(
+                  'Access by QR code',
+                  style: TextStyle(color: Color(0xff172633), fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                height: 130,
+              ),
+            ],
+          ),
         ),
       ),
     );
